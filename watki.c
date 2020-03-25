ae6476fc684e4a84fb4959fc9f21c3ba3bdcc97b
mmirek98 <michal.mirek98@gmail.com>
2020-03-25 19:31:48 +0100

    ex-4-setaffinity

--- watki.c
+++ watki.c
@@ -3,6 +3,8 @@
 #include <pthread.h>
 #include <limits.h>
 #include <linux/sched.h>
+#include <sched.h>
+
 #define _GNU_SOURCE
     #include <stdio.h>
        #include <stdlib.h>
@@ -53,6 +55,19 @@ void * zadanie_watku (void * arg_wsk) {
 }
 
 void zmierz_czas(void* nr_watku) {
+  // int s, j;
+  // cpu_set_t cpuset;
+  // pthread_t thread;
+  
+  // thread = pthread_self();
+
+  // CPU_ZERO(&cpuset);
+  // for (j = 0; j < 8; j++) {
+  //   CPU_SET(j, &cpuset);
+  // }
+
+  // pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+
   clock_t start = clock();
   czasozajmowacz();
   printf("Czas wykonywania: %lu ms dla watku %d \n", clock() - start, (int) nr_watku);
@@ -61,6 +76,18 @@ void zmierz_czas(void* nr_watku) {
 int main() {
   pthread_t tid[5];
   pthread_attr_t attr[5];
+
+  int s;
+  cpu_set_t cpuset;
+  pthread_t thread;
+  
+  thread = pthread_self();
+
+  CPU_ZERO(&cpuset);
+
+  CPU_SET(2, &cpuset);
+
+  pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
 
   // ==== pthread_attr_setstacksize 
   int i, j;
@@ -73,12 +100,12 @@ int main() {
   stack_size[4] = PTHREAD_STACK_MIN * PTHREAD_STACK_MIN;
 
   for (i = 0; i < 5; i++) {
-    pthread_attr_init(&attr[i]);
-    pthread_attr_setstacksize(&attr[i], stack_size[i]);
-    pthread_attr_getstacksize(&attr[i], &size[i]);
-    printf("Rozmiar stosu %d dla watku %d \n", size[i], i);
+    // pthread_attr_init(&attr[i]);
+    // pthread_attr_setstacksize(&attr[i], stack_size[i]);
+    // pthread_attr_getstacksize(&attr[i], &size[i]);
+    // printf("Rozmiar stosu %d dla watku %d \n", size[i], i);
 
-    pthread_create(&tid[i], &attr[i], (void*) zmierz_czas, (void*) i);
+    pthread_create(&tid[i], NULL, (void*) zmierz_czas, (void*) i);
     // pthread_join(tid[i], NULL);
   }
 
